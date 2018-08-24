e92e77b89ecb5121f2170ddd1f0fba3305787224
Simon Pugnet <simon@polaris64.net>
2018-08-24 21:57:50 +0200

    Modified runtime::NFPrintLn::execute() and example scripts
    The "print" and "println" NativeFunctions implemented by runtime allow
    printing of multiple arguments.  Therefore the scripts have been
    modified to take advantage of this.
    
    Modified the NFPrintLn::execute() implementation so that multiple
    arguments are first printed via print!(), followed by a newline via
    println!() at the end.

--- src/runtime.rs
+++ src/runtime.rs
@@ -27,12 +27,13 @@ impl NativeFunction for NFPrintLn {
     fn execute(&self, _scopes: &mut ScopeChain, args: &Vec<Value>) -> Value {
         for arg in args {
             match arg {
-                Value::Int(x) => println!("{}", x),
-                Value::Real(x) => println!("{}", x),
-                Value::Str(x) => println!("{}", x),
+                Value::Int(x) => print!("{}", x),
+                Value::Real(x) => print!("{}", x),
+                Value::Str(x) => print!("{}", x),
                 _ => println!("{:?}", arg),
             };
         }
+        println!("");
         Value::None
     }
     fn as_any(&self) -> &Any {
